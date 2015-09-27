54275ee2574a66f40b49f07a3d0c03ead7309f81
Paul <pavelmics@gmail.com>
2015-09-28 01:49:29 +0500

    little fix

--- web/index.php
+++ web/index.php
@@ -5,6 +5,6 @@ if (preg_match('/\.(?:png|gif|js|css)/', $_SERVER["REQUEST_URI"])) {
     return false;
 }
 
-$app = require('../app/app.php');
+$app = require(__DIR__ . '/../app/app.php');
 
 $app->run();
